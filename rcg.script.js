06b7b16223b520ed81faab6cc30b2bffda176fc1
Karan-Manhas <94873627+Karan-Manhas@users.noreply.github.com>
2021-12-06 19:50:43 +0000

    Update rcg.script.js
    Removing comments

--- rcg.script.js
+++ rcg.script.js
@@ -15,25 +15,7 @@ button.addEventListener('click', function() {
         return `rgb(${r}, ${g}, ${b})`;
     }
 
-//Colt Steele's js
 
-
-})
-/* const button = document.querySelector('button');
-const h1 = document.querySelector('h1');
-
-button.addEventListener('click', function () {
-    const newColor = makeRandColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
-})
-
-const makeRandColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
-} */ 
 
 
 
