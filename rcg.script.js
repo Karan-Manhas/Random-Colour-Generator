2237cfd9309682f0b312c90012e9dddda6ecbe71
Karan-Manhas <94873627+Karan-Manhas@users.noreply.github.com>
2021-12-06 19:49:09 +0000

    Adding files via upload

---
+++ rcg.script.js
@@ -0,0 +1,40 @@
+// This creates a button and stores the location of the button of HTML in JS
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+button.addEventListener('click', function() {  
+    const newColour = makeRandColour();
+    document.body.style.backgroundColor = newColour;
+    h1.innerText= newColour;
+
+
+    function makeRandColour() {
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+
+//Colt Steele's js
+
+
+})
+/* const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+button.addEventListener('click', function () {
+    const newColor = makeRandColor();
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+})
+
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+} */ 
+
+
+
+
